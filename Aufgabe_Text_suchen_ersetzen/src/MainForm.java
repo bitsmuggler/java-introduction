cd4e913f4078afacd95934f405e5b68b1fdc9953
Patrick Roos <pa.roos@icloud.com>
2016-03-09 15:33:43 +0100

    Grundaufgabe

---
+++ Aufgabe_Text_suchen_ersetzen/src/MainForm.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainForm extends JDialog {
+    private JPanel contentPane;
+    private JTextArea textAreaText;
+    private JTextField textFieldSearch;
+    private JTextField textFieldReplace;
+    private JButton buttonReplace;
+    private JButton beendenButton;
+    private JButton buttonOK;
+
+    public MainForm() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+        buttonReplace.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //Ersetze Text
+                String result = replaceText(
+                        textAreaText.getText(),
+                        textFieldSearch.getText(),
+                        textFieldReplace.getText());
+
+                //Setze den Text neu
+                textAreaText.setText(result);
+
+                //Eingabe-Felder zurücksetzen
+                textFieldSearch.setText("");
+                textFieldReplace.setText("");
+            }
+        });
+    }
+
+    /**
+     * Such im übergebenen Text nach einem bestimmten Wort und ersetzt das Wort anschliessend.
+     * @param currentText Aktueller Text
+     * @param searchWord Gesuchtes Wort
+     * @param replaceWord Ersetztes Wort
+     * @return Text mit ersetztem Wort
+     */
+    private String replaceText(String currentText, String searchWord, String replaceWord) {
+        return currentText.replace(searchWord, replaceWord);
+    }
+
+
+    public static void main(String[] args) {
+        MainForm dialog = new MainForm();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
