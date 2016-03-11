9bf7a28c1f5b8218c5e9b24aa33085855c47c900
Patrick Roos <pa.roos@icloud.com>
2016-03-11 10:37:49 +0100

    Grundaufgabe

--- Aufgabe_Quadrat_zeichnen/src/MainForm.java
+++ Aufgabe_Quadrat_zeichnen/src/MainForm.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JDialog {
     private JPanel contentPane;
-    private JSpinner spinner1;
+    private JSpinner spinnerNumberOfCols;
     private JButton OKButton;
     private JTextArea textAreaResult;
     private JButton buttonOK;
@@ -15,6 +17,12 @@ public class MainForm extends JDialog {
 
         Font font = new Font("Courier New", Font.PLAIN, 12);
         textAreaResult.setFont(font);
+        OKButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawRectangle();
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -24,4 +32,36 @@ public class MainForm extends JDialog {
         System.exit(0);
     }
 
+    /**
+     * Zeichnet ein Quadrat in der Textarea
+     */
+    private void drawRectangle() {
+        int cols, rows = cols = Integer.parseInt(spinnerNumberOfCols.getValue().toString());
+        System.out.println(cols);
+
+        //Zeilen durchgehen
+        for(int r = 0; r < rows; r++) {
+            String stars = "";
+
+            //Kolonnen durchgen
+            for(int c = 0; c < cols; c++) {
+
+                //Anfang - und Endzeile prüfen
+                if(r == 0 || r == (rows - 1)) {
+                    stars += "*";
+                } else {
+                    if(c == 0 || c == (cols - 1)) {
+                        stars += "*";
+                    } else {
+                        stars += " ";
+                    }
+                }
+            }
+
+            //Zeile für Zeile ausgeben
+            textAreaResult.append(stars + "\n");
+            System.out.println(stars);
+        }
+
+    }
 }
